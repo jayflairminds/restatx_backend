ebacef9179e73c3f46b15602257ab90f7111a95e
jayflairminds <jay.sharma@flairminds.com>
2024-07-15 15:48:19 +0530

    added project_name to the response

--- construction/views.py
+++ construction/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from users.models import UserProfile
 from .serializers import *
@@ -18,8 +19,30 @@ class LoanListView(generics.ListAPIView):
         print(profile.role_type)
         match profile.role_type:
             case "lender":
-                return Loan.objects.filter(lender_id=user)
+                loans = Loan.objects.filter(lender_id=user)
             case "inspector":
-                return Loan.objects.filter(inspector_id=user)
+                loans = Loan.objects.filter(inspector_id=user)
             case "borrower":
-                return Loan.objects.filter(borrower_id=user)
+                loans = Loan.objects.filter(borrower_id=user)
+
+        # for loan in loans:
+        #     project_id = loan.project_id
+        #     project = Project.objects.filter(id= project_id)
+        #     loan.project_name = project.values
+        loans = loans.select_related('project')
+        return loans
+    
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+        data = response.data
+        
+        # Specify the keys you want to include in the output
+        allowed_keys = {'loanid', 'projectname', 'address', 'loandescription', 'amount', 'status','loantype',  'start_date', 'end_date', 'interestrate', 'duration', 'borrower', 'lender', 'inspector', 'project'}
+        
+        # Filter the data to include only the allowed keys
+        filtered_data = [
+            {key: value for key, value in item.items() if key in allowed_keys}
+            for item in data
+        ]
+        
+        return Response(filtered_data)
