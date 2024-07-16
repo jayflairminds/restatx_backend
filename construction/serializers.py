4d96442c708f73e86b30beba075d30cdb6391f96
jayflairminds <jay.sharma@flairminds.com>
2024-07-16 11:38:58 +0530

    updating the response of asset-list api

--- construction/serializers.py
+++ construction/serializers.py
@@ -11,7 +11,9 @@ class ProjectSerializer(serializers.ModelSerializer):
 class LoanSerializer(serializers.ModelSerializer):
     projectname = serializers.CharField(source="project.projectname", read_only=True)
     address = serializers.CharField(source="project.address", read_only=True)
-
+    borrower_name = serializers.CharField(source="borrower.username", read_only=True)
+    lender_name = serializers.CharField(source="lender.username", read_only=True)
+    inspector_name = serializers.CharField(source="inspector.username", read_only=True)
     class Meta:
         model = Loan
         fields = "__all__"
