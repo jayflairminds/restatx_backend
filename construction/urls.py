b3a57ab51c9d04c4e0281e59ebaf583d3ade8937
jayflairminds <jay.sharma@flairminds.com>
2024-07-17 18:54:21 +0530

    creating update status endpoint

--- construction/urls.py
+++ construction/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoanListView,LoanDisbursementScheduleDetail,BudgetDetail
+from .views import LoanListView,LoanDisbursementScheduleDetail,BudgetDetail,UpdateDisbursementStatus
 
 urlpatterns = [
     path("asset-list/", LoanListView.as_view(), name="asset-list"),
     path("disbursement-detail/", LoanDisbursementScheduleDetail.as_view(), name="disbursement-list"),
-    path("budget-detail/",BudgetDetail.as_view(), name="budget-detail")
+    path("budget-detail/",BudgetDetail.as_view(), name="budget-detail"),
+    path("update-disbursement-status/",UpdateDisbursementStatus.as_view(),name="update-disbursement-status")
 ]
