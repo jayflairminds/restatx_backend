89d96f4efac63bd4f3671e0359bb3fcd6ab87fc8
jayflairminds <jay.sharma@flairminds.com>
2024-07-18 20:18:54 +0530

    changes in update status endpoint

--- construction/views.py
+++ construction/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Loan
 from .serializers import LoanSerializer
+import datetime
 
 
 class LoanListView(generics.ListAPIView):
@@ -97,13 +98,50 @@ class UpdateDisbursementStatus(APIView):
         status_action = input_param.get('status_action')
         user = request.user
         profile = UserProfile.objects.get(user=user)
+
+        if profile.role_type == "borrower":
+            if status_action == "Request for Disbursement":
+                update_status = "Pending Inspection"
+                my_instance = LoanDisbursementSchedule.objects.get(pk=loan_disbursment_id)
+                my_instance.date_requested = datetime.datetime.now()
+                my_instance.save(update_fields=['date_requested'])
+        elif profile.role_type == "inspector":
+            if status_action == "Approve":
+                update_status = "Pending Disbursement"
+            if status_action == "Request More Information from Borrower":
+                update_status = "Request More Information from Borrower"
+        elif profile.role_type == "lender":
+            if status_action == "Request Information from Inspector":
+                update_status = "Request Information from Inspector"
+            if status_action == "Request Information from Borrower":
+                update_status = "Request Information from Borrower"
+            if status_action == "Approve":
+                update_status = "Approved"
+            if status_action == "Complete":
+                update_status = "Completed"
         try:
-            loan_disbursement = LoanDisbursementSchedule.objects.get( loan_disbursment_id=loan_disbursment_id)
+            my_instance = LoanDisbursementSchedule.objects.get(pk=loan_disbursment_id)
+            my_instance.disbursement_status = update_status
+            my_instance.save(update_fields=['disbursement_status'])
+            return Response(status=status.HTTP_200_OK)
         except:
             return Response({'error': 'Record not found'}, status=status.HTTP_404_NOT_FOUND)
-        print(loan_disbursement)
-        my_instance = LoanDisbursementSchedule.objects.get(pk=loan_disbursment_id)
-        my_instance.disbursement_status = status_action
-        my_instance.save(update_fields=['disbursement_status'])
-        return Response(status=status.HTTP_200_OK)
         
+
+# class ReturnDisbursementStatusMapping(APIView):
+#     serializer_class = LoanDisbursementScheduleSerializer
+#     permission_classes = [IsAuthenticated]
+
+#     def get(self, request):
+#         input_param = request.data
+#         loan_disbursment_id = input_param.get('loan_disbursment_id')
+#         status_action = input_param.get('status_action')
+#         user = request.user
+#         profile = UserProfile.objects.get(user=user)
+#         role_type = profile.role_type
+#         try:
+#             loan_disbursement = LoanDisbursementSchedule.objects.get( loan_disbursment_id=loan_disbursment_id)
+#         except:
+#             return Response({'error': 'Record not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+#         return Response(status=status.HTTP_200_OK)
