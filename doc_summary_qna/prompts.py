c59f3906397a01899a40291b41a37fa5ddbdddb0
shivanikulkarni09 <shivakulkarni0901@gmail>
2024-08-22 14:51:25 +0530

    changes in prompt template

--- doc_summary_qna/prompts.py
+++ doc_summary_qna/prompts.py
@@ -2,9 +2,9 @@
 # defines the prompts under predefined prompt buttons
 def predefined_prompts():
     user_question="""
-    Please provide a detailed summary of the content in the document. 
-    Include the main topics, key points, and any significant details or findings mentioned in the document.
-    Ensure that the summary is comprehensive and covers all major sections of the PDF. 
+    Summarize the document's content. Include the main topics, key points, and any important details or findings. 
+    Make sure to cover all major sections. 
+    Don't give empty "" response.
     """
 
     return user_question
