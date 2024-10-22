bfe294e58c5d346108683087843219f5a066cf56
jayflairminds <jay.sharma@flairminds.com>
2024-10-22 17:16:10 +0530

    changes in disbursement table

--- construction/helper_functions.py
+++ construction/helper_functions.py
@@ -15,32 +15,31 @@ def disbursement_schedule(loan_id):
     normalized_months = (months - duration / 2) / (duration / 10)
 
     s_curve_values = sigmoid(normalized_months)
-    print("total_loan_budget",total_loan_budget['total_loan_budget'])
-    scaled_s_curve_values = s_curve_values * total_loan_budget['total_loan_budget']
     
+    scaled_s_curve_values = s_curve_values * total_loan_budget['total_loan_budget']
+    total_sum_curve_value = np.sum(scaled_s_curve_values)
     draw_request_dict = {item['draw_request']: item['total_released_amount'] for item in draws_obj_lis}
-    print('draw_request_dict',draw_request_dict)
-    output_lis = create_disbursement_schedule_output_json(months, scaled_s_curve_values, draw_request_dict)
+    output_lis = create_disbursement_schedule_output_json(months, scaled_s_curve_values, draw_request_dict,total_loan_budget['total_loan_budget'])
     return output_lis
 
 def sigmoid(amount):
     return 1 / (1 + np.exp(-amount))
 
-def create_disbursement_schedule_output_json(months,scaled_s_curve_values,draw_request_dict):
+def create_disbursement_schedule_output_json(months,scaled_s_curve_values,draw_request_dict,total_sum_curve_value):
     input_lis = list(zip(months, scaled_s_curve_values))
-    print(input_lis)
+    
     output_lis = list()
     for i,j in input_lis: 
-        print(i,j)
+
         output_dictionary = {
-            "theoretical": j,
+            "theoretical": (j/total_sum_curve_value)*100,
             "review_month":i
         }
          # If draw_request (from DrawTracking) matches the review_month
         if i in draw_request_dict:
-            output_dictionary["actual"] = draw_request_dict[i]  # Add actual key with total_released
+            output_dictionary["actual"] = (draw_request_dict[i] /total_sum_curve_value)*100  # Add actual key with total_released
         else:
-            output_dictionary["actual"] = 0
+            output_dictionary["actual"] = None
         output_lis.append(output_dictionary)
     return output_lis
 
